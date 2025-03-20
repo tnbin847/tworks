462cf295ce9cc93d5fd7c5ff0f8b3551e769b491
tnbin847 <tnqls3718@gmail.com>
2025-03-20 10:14:00 +0900

    최초 커밋

---
+++ src/test/java/com/suhin/tworks/WebAppTests.java
@@ -0,0 +1,13 @@
+package com.suhin.tworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
