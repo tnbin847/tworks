019a81caa626fc00fef5e71204ddf20164046e2b
tnqls847 <tnqls3718@gmail.com>
2025-03-21 12:58:39 +0900

    refactor: 공통 css 작성중

--- src/main/java/com/suhin/tworks/WebApp.java
+++ src/main/java/com/suhin/tworks/WebApp.java
@@ -2,7 +2,10 @@ package com.suhin.tworks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 @SpringBootApplication
 public class WebApp {
 
@@ -10,4 +13,18 @@ public class WebApp {
 		SpringApplication.run(WebApp.class, args);
 	}
 
+	@GetMapping("/")
+	public String index() {
+		return "pages/index";
+	}
+
+	@GetMapping("/sign-up")
+	public String signUp() {
+		return "pages/user/signup";
+	}
+
+	@GetMapping("/find")
+	public String findUser() {
+		return "pages/user/find";
+	}
 }
