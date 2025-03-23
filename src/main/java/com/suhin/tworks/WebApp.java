057613c132681ec8209f0789abf29696fbe37530
tnbin847 <tnqls3718@gmail.com>
2025-03-23 21:21:13 +0900

    refactor: project last reset.

--- src/main/java/com/suhin/tworks/WebApp.java
+++ src/main/java/com/suhin/tworks/WebApp.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebApp {
+
 	public static void main(String[] args) {
 		SpringApplication.run(WebApp.class, args);
 	}
+
 }
